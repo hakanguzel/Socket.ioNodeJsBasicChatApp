6c61e64aa952c6aa6a9c6523fc5d84e6716919dc
Hakan GÜZEL <info@hakanguzel.com>
2021-09-02 09:49:58 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const port = process.env.PORT || 3000;
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log('listening on'+port);
+});
